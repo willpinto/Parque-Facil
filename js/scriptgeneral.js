1a01abfa4a3f21e08c2044864d74a90502b6e421
Nib-RA <elniborson@gmail.com>
2020-11-27 15:05:15 -0500

    Cambios generales

--- js/scriptgeneral.js
+++ js/scriptgeneral.js
@@ -6,11 +6,26 @@ $(document).ready(function() {
     });
 
     $('#ConfirmarIngresar').click(function() {
-        $('#login-modal').modal('hide');
-        $('#temp-modal').modal('show');
+        if($('#user').val() != "12345" || $('#pass').val() != "12345") {
+            alert("Documento y/o contraseña incorrectas");
+        } else {
+            $('#login-modal').modal('hide');
+            $('#temp-modal').modal('show');
+        }
     });
 
     $('#registrar-propietario').click(function() {
         $('#tc-modal').modal('show');
+    });
+
+    $('#aceptar-tc').change(function() {
+        $('#ConfirmarRegistroProp ').toggleClass('disabled');
+    });
+
+    $('#chkBici').change(function() {
+        $('#chasis').toggleClass('disabled');
+        $('#motor').toggleClass('disabled');
+        $('#cilindraje').toggleClass('disabled');
+        $('#carroceria').toggleClass('disabled');
     });
 });
