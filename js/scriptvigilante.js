0c41788e3e3d7e7e2c9cdd5645b892f0df1f22a0
Nib-RA <elniborson@gmail.com>
2020-11-27 15:05:33 -0500

    Se mejoro la interaccion de botones

--- js/scriptvigilante.js
+++ js/scriptvigilante.js
@@ -23,4 +23,23 @@ $(document).ready(function() {
         $('#checkout-manual-modal').modal('hide');
         $('#checkout-info-modal').modal('show');
     });
+
+    $('#btnValidarPlaca').click(function() {
+        if($('#placa').val() == "ABC-123") {
+            alert("El vehículo con placa ABC-123 esta actualmente registrado en el sistema y esta activo");
+            $('#novedad').removeAttr('disabled');
+            $('#btnRegistrarNovedad').removeAttr('disabled');
+        } else {
+            alert("El vehículo con placa " + $('#placa').val() + " no se encuentra actualmente activo");
+        }
+    });
+
+    $('#btnRegistrarNovedad').click(function() {
+        if($('#novedad').val() != 99){
+            alert("Se ha registrado la novedad al vehículo con placa ABC-123");
+            location.href = "vigilanteindex.html";
+        } else {
+            alert("No se ha seleccionado ninguna novedad");
+        }
+    })
 });
